1f95b3c06f8676398ceac6dc44877dbc39caec91
Peter Hodge <peter.hodge84@gmail.com>
2021-12-12 06:13:02 +1000

    Draft setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='python-ansible-wrapper',
+    version='0.0.0',
+    description='',
+    packages=['python_ansible_wrapper'],
+    package_data={'python_ansible_wrapper': ['py.typed', '**/py.typed']},
+    install_requires=[
+        'pyyaml',
+        'chromalog',
+    ],
+)
